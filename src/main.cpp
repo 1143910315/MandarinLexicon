84dad56cc9ac434549185e3916788253b9e743cd
HapiFive <1143910315@qq.com>
2024-10-21 15:56:40 +0800

    项目初始化配置

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,9 @@
+#include <format>
 #include <iostream>
 
-int main(int argc, char** argv) {
-    std::cout << "hello world!" << std::endl;
+int main(int argc, char **argv) {
+    (void)argc;
+    (void)argv;
+    std::cout << std::format("hello world!{}", "") << std::endl;
     return 0;
 }
